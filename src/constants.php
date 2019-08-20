4a674dab79bf2dc1cac40660d51f46dfcd5bb8b3
Nikita Orlov <norlov@archive.systems>
2019-08-20 16:37:17 +0300

    fix data path

--- src/constants.php
+++ src/constants.php
@@ -4,5 +4,5 @@ defined('APPLICATION_ENV') || define('APPLICATION_ENV', (getenv('APPLICATION_ENV
 
 defined('APPLICATION_PATH') || define('APPLICATION_PATH', realpath(__DIR__ . '/application'));
 defined('ZFE_PATH')         || define('ZFE_PATH',         realpath(__DIR__ . '/vendor/zfbase/zfe/library/ZFE'));
-defined('DATA_PATH')        || define('DATA_PATH',        realpath(__DIR__ . '/data'));
+defined('DATA_PATH')        || define('DATA_PATH',        realpath(__DIR__ . '/../data'));
 defined('ASSETS_PATH')      || define('ASSETS_PATH',      realpath(__DIR__ . '/assets'));
