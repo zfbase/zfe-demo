fedbded052b117fb7ba275f759c9339718675942
Nikita Orlov <norlov@archive.systems>
2019-08-27 19:30:52 +0300

    вернул все в энтрипоинт

--- src/docker-entrypoint.sh
+++ src/docker-entrypoint.sh
@@ -5,9 +5,9 @@ set -e
 
 # php-fpm work under www-data
 chmod -R 777 ../* && chown -R www-data:www-data ../*
-#composer install --prefer-source --no-interaction --no-plugins --no-scripts
-#npm install --only=production --no-audit
-#npm run build
+composer install --prefer-source --no-interaction --no-plugins --no-scripts
+npm install --only=production --no-audit
+npm run build
 
 # @see https://github.com/docker-library/php/blob/135204c0f4c7a255ee70ec82dc1133929f9676c0/7.3/buster/fpm/docker-php-entrypoint
 if [ "${1#-}" != "$1" ]; then
